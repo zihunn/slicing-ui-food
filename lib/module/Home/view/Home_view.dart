4fd1f037777e43d3ddcbf9c15438e687180e89e8
zihunn <muhammadrizky@smkinformatika-crbn.sch.id>
2023-07-31 14:04:36 +0700

    update

--- lib/module/Home/view/Home_view.dart
+++ lib/module/Home/view/Home_view.dart
@@ -524,33 +524,3 @@ class BNBCustomPainter extends CustomPainter {
     return false;
   }
 }
-
-class CardClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    var roundnessFactor = 50.0;
-
-    var path = Path();
-
-    path.moveTo(0, size.height * 0.33);
-    path.lineTo(0, size.height - roundnessFactor + 10);
-    path.quadraticBezierTo(0, size.height, roundnessFactor, size.height);
-    path.lineTo(size.width - roundnessFactor, size.height);
-    path.quadraticBezierTo(
-        size.width, size.height, size.width, size.height - roundnessFactor);
-    path.lineTo(size.width, roundnessFactor * 2);
-    path.quadraticBezierTo(size.width - 5, roundnessFactor,
-        size.width - roundnessFactor * 1.5, roundnessFactor * 1.5);
-    path.lineTo(
-        roundnessFactor * 0.6, size.height * 0.33 - roundnessFactor * 0.3);
-    path.quadraticBezierTo(
-        0, size.height * 0.33, 0, size.height * 0.33 + roundnessFactor);
-
-    return path;
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-    return true;
-  }
-}
